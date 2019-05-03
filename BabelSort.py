39616f51812b12fc2f0dc06c15187b29d90ceb71
Agnieszka <agulab7@interia.pl>
2019-05-03 18:16:47 +0200

    dodano selection sort

--- BabelSort.py
+++ BabelSort.py
@@ -7,13 +7,24 @@ def babel(t):
                 t[i],t[i+1] = t[i+1],t[i]
     return t
 ########################
+def selection(t):
+    n=len(t)
+    for i in range(n-1):
+        k=i
+        for j in range(i+1, n):
+            if t[j]<t[k]:
+                k=j
+        t[k],t[i] = t[i], t[k]
+    return t
+
 t = []
 for i in range(0,5):
     x = random.randint(0,100) #random.random() - liczba z zakresu 0-1
     t.append(x)
 ##############
 print(t)
-babel(t)
+#babel(t)
+selection(t)
 print(t)
 
 
